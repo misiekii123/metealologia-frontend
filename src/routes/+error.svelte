019703884c768aeeee68d93540f63a5ca4ba9e45
Asapros <asapros@wp.pl>
2024-07-25 02:08:14 +0200

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import {page} from "$app/stores";
+</script>
+
+<h1>Błąd {$page.status}</h1>
+<h2>{$page.error?.message}</h2>
+
+{#if $page.status >= 500}
+    <h2>Spróbuj ponownie później</h2>
+{/if}
