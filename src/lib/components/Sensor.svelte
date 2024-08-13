57afd7d6cac08a565c58033ee7e7a5e4d9e7c565
misiekii123 <92684568+misiekii123@users.noreply.github.com>
2024-08-13 22:59:58 +0200

    humidity sensor support

--- src/lib/components/Sensor.svelte
+++ src/lib/components/Sensor.svelte
@@ -35,6 +35,8 @@
         <LineChart value="Ciśnienie [hPa]" reports={reports} range={{min: 900, max: 1100}} dataType={[data.type]} fill={true} legend={false}/>
     {:else if data.type === "air-polution"}
         <LineChart value="Zanieczyszczenie powietrza [µg/m³]" reports={reports} range={{min: 0, max: 100}} dataType={["pm1.0", "pm2.5", "pm10"]} fill={false} legend={true}/>
+    {:else if data.type === "humidity"}
+        <LineChart value="Wilgotność [%]" reports={reports} range={{min: 0, max: 100}} dataType={[data.type]} fill={false} legend={false}/>
     {:else}
         <p>Typ czujnika '{data.type}' nie jest obsługiwany przez stronę.</p>
     {/if}
