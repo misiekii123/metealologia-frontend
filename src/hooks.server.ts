e0cca1f7f237b789ac613a6aeaed8f9b47d6e330
Asapros <asapros@wp.pl>
2024-09-21 19:58:50 +0200

    access logs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,10 @@
-// /** @type {import('@sveltejs/kit').Handle} */
-// export async function handle({ event, resolve }) {
-//     console.log(event.url.href);
-//     return await resolve(event);
-// }
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+    const now = new Date().toISOString().split(".")[0].replace("T", " ")
+    const response = await resolve(event);
+    console.log(`${now} access - "${event.request.method} ${event.url.pathname}" ${response.status}`)
+    return response
+}
 
 /** @type {import('@sveltejs/kit').HandleServerError} */
 export async function handleError({ error, event, status, message }) {
