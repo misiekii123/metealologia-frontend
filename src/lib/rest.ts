981c31dc11b6bce1f4dd1014c5e5693471aa1a98
Asapros <asapros@wp.pl>
2024-09-21 19:53:25 +0200

    buildtime env fix

--- src/lib/rest.ts
+++ src/lib/rest.ts
@@ -1,10 +1,6 @@
 import {env} from "$env/dynamic/public";
 import {error} from "@sveltejs/kit";
 
-const OVERVIEW_URL = new URL("", env.PUBLIC_BACKEND_URI).href;
-const STATIONS_URL = new URL("stations", env.PUBLIC_BACKEND_URI).href;
-const REPORTS_URL = new URL("/stations/(station)/sensors/(sensor)/reports?after=(after)&before=(before)", env.PUBLIC_BACKEND_URI).href;
-
 export interface SensorReport {
     timestamp: string;
     data: any;
@@ -41,14 +37,17 @@ async function wrappedFetch(url: string, fetchFunction: Function) {
 }
 
 export async function fetchBackendMeta(fetchFunction: Function = fetch): Promise<BackendMeta> {
+    const OVERVIEW_URL = new URL("", env.PUBLIC_BACKEND_URI).href;
     return await wrappedFetch(OVERVIEW_URL, fetchFunction);
 }
 
 export async function fetchStations(fetchFunction: Function = fetch): Promise<StationMeta> {
+    const STATIONS_URL = new URL("stations", env.PUBLIC_BACKEND_URI).href;
     return await wrappedFetch(STATIONS_URL, fetchFunction);
 }
 
 export async function fetchReports(station: string, sensor: string, before: Date, after: Date, fetchFunction: Function = fetch): Promise<SensorReport[]> {
+    const REPORTS_URL = new URL("/stations/(station)/sensors/(sensor)/reports?after=(after)&before=(before)", env.PUBLIC_BACKEND_URI).href;
     const url = REPORTS_URL
         .replace("(station)", station)
         .replace("(sensor)", sensor)
